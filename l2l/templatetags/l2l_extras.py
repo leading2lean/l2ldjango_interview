5b6660a13283d6910849c9ae93ef68a26935ff07
Chad Carey <ccarey@xoi.io>
2022-07-13 21:28:28 -0500

    removed unneeded comment

--- l2l/templatetags/l2l_extras.py
+++ l2l/templatetags/l2l_extras.py
@@ -6,10 +6,6 @@ from typing import (
 
 @register.filter(name='l2l_dt_format')
 def l2l_dt_format(value: Union[datetime, str]) -> str:
-    '''
-    Takes a date in format: "%Y-%m-%dT%H:%M:%S"
-    Returns format: "%Y-%m-%d %H:%M:%S".
-    '''
     if isinstance(value, str):
         value = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S')
     return value.strftime('%Y-%m-%d %H:%M:%S')
