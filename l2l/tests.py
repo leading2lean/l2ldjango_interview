12b94b44aadde2c6c7d6740f35ffa77f8fba570e
Chad Carey <ccarey@xoi.io>
2022-07-13 21:36:05 -0500

    added tests

--- l2l/tests.py
+++ l2l/tests.py
@@ -1,3 +1,16 @@
+from datetime import datetime
 from django.test import TestCase
+from l2l.templatetags import l2l_extras
 
-# Create your tests here.
+
+class L2lExtrasTests(TestCase):
+    def test_l2l_dt_format_formats_dt_string(self):
+        expected = '2022-07-14 02:25:10'
+        actual = l2l_extras.l2l_dt_format('2022-07-14T02:25:10')
+        self.assertEqual(actual, expected)
+
+    def test_l2l_dt_format_formats_dt(self):
+        expected = '2022-07-14 02:25:10'
+        dt = datetime.strptime('2022-07-14T02:25:10', '%Y-%m-%dT%H:%M:%S')
+        actual = l2l_extras.l2l_dt_format(dt)
+        self.assertEqual(actual, expected)
