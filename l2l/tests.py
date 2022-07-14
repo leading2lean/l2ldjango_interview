a3fb5f400cc61a1a012d05f5c243302363097190
Chad Carey <ccarey@xoi.io>
2022-07-13 21:37:09 -0500

    test fix

--- l2l/tests.py
+++ l2l/tests.py
@@ -11,6 +11,6 @@ class L2lExtrasTests(TestCase):
 
     def test_l2l_dt_format_formats_dt(self):
         expected = '2022-07-14 02:25:10'
-        dt = datetime.strptime('2022-07-14T02:25:10', '%Y-%m-%dT%H:%M:%S')
+        dt = datetime.strptime(expected, '%Y-%m-%d %H:%M:%S')
         actual = l2l_extras.l2l_dt_format(dt)
         self.assertEqual(actual, expected)
